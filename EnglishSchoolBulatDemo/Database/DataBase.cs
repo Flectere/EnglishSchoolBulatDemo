b102ea7e77847acbd033b6e6c73dff1e61ac72ea
Sofiya.Kumyshbaeva <Sofiya.Kumyshbaeva@K1210-0014>
2024-09-30 19:10:01 +0300

    Добавьте файлы проекта.

---
+++ EnglishSchoolBulatDemo/Database/DataBase.cs
@@ -0,0 +1,14 @@
+﻿using EnglishSchoolBulatDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishSchoolBulatDemo.Database
+{
+    internal class DataBase
+    {
+        public static EnglishSchoolBulatDemoEntities db = new EnglishSchoolBulatDemoEntities();
+    }
+}
